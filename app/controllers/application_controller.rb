f05ccbdbf0f1e29c5723649f9f3644816e435ba2
Johnny Ibarra <Enjoi18@ymail.com>
2018-11-26 20:02:42 -0700

    finished subs controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+	# before_action :verify_paid
+
+	def verify_paid 
+		@paid = true
+	end
+
+	# this is good for 'User Login'
 end
--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -4,25 +4,104 @@ class SubsController < ApplicationController
 		# D.R.Y and only worry about actions
 		# CRUD - CREATE READ UPDATE and DELETE
 
-		# index @ppl = Ppl.all
-		# new @person = Ppl.new
-		# create @person = Ppl.create(ppl_params)
-		# edit @person = Ppl.find(parms[:id])
-		# update @person = Ppl.find(params[:id]).update(ppl_params)
-		# delete @person = Ppl.find(params[:id]).destroy
+		# index @ppl = Person.all
+		# show @person = Person.find(params[:id])
+		# new @person = Person.new
+		# create @person = Person.create(ppl_params)
+		# edit @person = Person.find(parms[:id])
+		# update @person = Person.find(params[:id]).update(ppl_params)
+		# delete @person = Person.find(params[:id]).destroy
+		
+		#callbacks
+			# before_action, called before entering a method
+			# after_action, called after entering a method 
+			# skip_before_action, skips before action
+			# skip_after_action, skips the after actions 
+
+			# before_action :method_to_call, (only/except): [:index, :show]
+
 
 	# Model, all logical code
 		# ordering, printing
 
-  def index
+	before_action :set_sub, only: [:show, :update, :edit, :destroy]
+	# before_action :set_sub, except [:index, :new, :create]
+
+
+	# HOW TO DO RAKE NOTES 
+	# IN TERMINAL TYPE BUNDLE EXEC RAKE NOTES
+
+		# TODO anything thas needs to be done
+		# FIXME fix bugs or errors
+		# OPTIMIZE refactor
+
+		# TODO fill out index views 
+		# FIXME  page not loading
+		# OPTIMIZE change the color of the text 
+
+	def index
+		@subs = Sub.all
+		# This also renders a webpage/views
+		# views/subs/index.html.erb
+
+		#default render html
+		# but you can also render json, xml or redirect 
   end
 
-  def show
+	#gets
+	def show
+		# default renders show views/shows/sub/show.html
   end
 
-  def new
-  end
+	# get request
+	def new
+		# creates a sub in memory
+		@sub = Sub.new
+		# renders the sub form
+		render partial: "form"
+	end
+	
+	# post / creates something into the database
+	def create
+		@sub = Sub.create(sub_params)
+		if @sub.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
 
-  def edit
-  end
+	# get
+	def edit
+		#@sub = Sub.find(params[:id])
+		# render the edit form
+		render partial: "form"
+	end
+	
+	#put
+	def	update
+		#@sub = Sub.find(params[:id])
+		if @sub.update(sub_params)
+			redirect_to @sub
+		else
+			render :edit 
+		end
+	end
+
+	# delete
+	def destroy
+	#	@sub = Sub.find(params[:id])
+		@sub.destroy
+		redirect_to root_path
+	end
+
+	private
+		def sub_params
+			params.require(:sub).permit(:name)
+		end
+
+		def set_sub
+			@sub = Sub.find(params[:id])
+		end
+
 end
